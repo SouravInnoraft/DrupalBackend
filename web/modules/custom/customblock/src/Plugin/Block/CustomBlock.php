2e430c8367c62ccf72aab19606a218dadfc55c3b
Sourav_Innoraft <sourav.chandra@innoraft.com>
2024-08-05 12:08:57 +0530

    FT2-22: Added blocks.

---
+++ web/modules/custom/customblock/src/Plugin/Block/CustomBlock.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\customblock\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a customblock block.
+ *
+ * @Block(
+ *   id = "customblock_customblock",
+ *   admin_label = @Translation("customblock"),
+ *   category = @Translation("Custom"),
+ * )
+ */
+class CustomBlock extends BlockBase
+{
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build()
+  {
+    $current_user = \Drupal::currentUser();
+    $name = $current_user->getDisplayName();
+    // $role = !empty($roles) ? reset($roles) : 'Guest';
+
+    return [
+      '#markup' => $this->t('Welcome @name', ['@name' => $name]),
+    ];
+  }
+}
